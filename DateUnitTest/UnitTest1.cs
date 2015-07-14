e34abe771b20c23078c8e818077b6160914e1da4
idomaz00 <idomaz00@fesb.hr>
2015-07-14 17:01:39 +0200

    Drugi domaci rad - unit testovi

---
+++ DateUnitTest/UnitTest1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DomaciRad2Testiranje;
+
+namespace DateUnitTest
+{
+    [TestClass]
+    public class DateTesting
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void TestWrongYearValue()
+        {
+            var datum = new Date(0,2,2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void TestWrongMonthValue()
+        {
+            var datum = new Date(2000, 16, 4);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void TestWrongDayValue()
+        {
+            var datum = new Date(2000, 5, 70);
+        }
+
+        [TestMethod]
+        public void TestReturnMonthNameValue()
+        {
+            var datum = new Date(2001, 6, 1);
+
+            Assert.AreEqual("Lipanj", datum.getMonthName(datum), "6.mjesec je Lipanj");  
+        }
+
+        [TestMethod]
+        public void TestReturnRemainingDaysValid30Value()
+        {
+            var datum = new Date(2001, 6, 25);
+
+            Assert.AreEqual(5, datum.getNumberOfRemainingDaysInMonth(datum), "Lipanj ima 30 dana, znaci da je preostali broj dana 5");
+        }
+
+        [TestMethod]
+        public void TestReturnRemainingDaysValid31Value()
+        {
+            var datum = new Date(2001, 10, 14);
+
+            Assert.AreEqual(17, datum.getNumberOfRemainingDaysInMonth(datum), "Listopad ima 31 dan, znaci da je preostali broj dana 17");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void TestReturnRemainingDaysInvalidValue()
+        {
+            var datum = new Date(2001, 9, 31);
+
+            int brojPreostalihDana = datum.getNumberOfRemainingDaysInMonth(datum);
+        }
+
+        [TestMethod]
+        public void TestReturnRemainingDaysForLeapYearValidValue()
+        {
+            var datum = new Date(2000, 2, 25);
+
+            Assert.AreEqual(4, datum.getNumberOfRemainingDaysInMonth(datum), "Krivi izračun broja preostalih dana u prijestupnoj godini i mjesecu Veljači");
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void TestReturnRemainingDaysForLeapYearInvalidDayValue()
+        {
+            var datum = new Date(2004, 2, 30);
+
+            int brojPreostalihDana = datum.getNumberOfRemainingDaysInMonth(datum);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void TestReturnRemainingDaysForNoneLeapYearInvalidDayValue()
+        {
+            var datum = new Date(2003, 2, 30);
+
+            int brojPreostalihDana = datum.getNumberOfRemainingDaysInMonth(datum);
+        }
+
+        [TestMethod]
+        public void TestReturnRemainingDaysForNoneLeapYearFebruaryValidValue()
+        {
+            var datum = new Date(2003, 2, 25);
+
+            Assert.AreEqual(3, datum.getNumberOfRemainingDaysInMonth(datum), "Preostalih dana u neprijestupnoj godini i mjesecu Veljači nakon 25. dana treba biti 3");
+        }
+
+    }
+}
---
+++ DomaciRad2Testiranje/Date.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomaciRad2Testiranje
+{
+    public class Date
+    {
+        private string[] mjeseciUGodini = {"Sijecanj","Veljaca","Ozujak","Travanj","Svibanj","Lipanj","Srpanj","Kolovoz","Rujan","Listopad","Studeni","Prosinac"} ;
+        private int godina, mjesec, dan;
+
+        public Date(int godina, int mjesec, int dan)
+        {
+            if (godina!=0 && mjesec < 13 && mjesec > 0 && dan < 32 && dan > 0)
+            {
+                this.godina = godina;
+                this.mjesec = mjesec;
+                this.dan = dan;
+            }
+            else
+            {
+                throw new InvalidProgramException("Krivi unos vrijednosti za mjesec i/ili dan"); 
+            }
+        }
+
+        public string getMonthName(Date datum)
+        {
+            return mjeseciUGodini[datum.mjesec - 1];
+        }
+
+        public int getNumberOfRemainingDaysInMonth(Date datum)
+        {
+            switch (datum.mjesec)
+            {
+                case 1: 
+                case 3: 
+                case 5: 
+                case 7: 
+                case 8: 
+                case 10:
+                case 12:
+                        return 31 - datum.dan; //uvijek ce biti manji ili jednak 31 za to se brine konstruktor
+                case 4: 
+                case 6: 
+                case 9:
+                case 11: 
+                    if(datum.dan != 31) // ne smije biti jednak 31, a da je manji tj da nije veci je briga konstruktora
+                        return 30 - datum.dan;
+                    else
+                        throw new InvalidProgramException("Dan ne smije biti veći od 30");
+                case 2:
+                    if (isLeapYear(datum.godina) && datum.dan < 30)
+                        return 29 - datum.dan;
+                    else if (!isLeapYear(datum.godina) && datum.dan < 29)
+                        return 28 - datum.dan;
+                    else
+                        throw new InvalidProgramException("Kriv unos dana za mjesec Veljaču");
+                default:
+                    throw new InvalidProgramException("Krivi unos mjeseca"); 
+
+            }
+            
+        }
+
+        private bool isLeapYear(int godina)
+        {
+            if (godina%4==0 && (godina%100!=0 || godina%400==0)) 
+                return true;
+            else
+                return false;
+        }
+    }
+}
